c91e8244cd3f20a3fe4331ae819c09c2b2424611
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-11-18 17:32:33 +0300

    Added practice 6

---
+++ src/com/tsekhanovich/functional/practice6/Task1.java
@@ -0,0 +1,27 @@
+package com.tsekhanovich.functional.practice6;
+
+import java.util.function.Function;
+
+/**
+ * @author Pavel Tsekhanovcih 10.11.2018
+ *
+ * Using closure write a lambda expression that calculates a ∗ x2 + b ∗ x + c where a, b, c are
+ * context final variables. They will be available in the context during testing. Note, the result is double.
+ * Solution format. Submit your lambda expression in any valid format with ; on the end.
+ *
+ * Examples: (x, y) -> x + y; (x, y) -> { return x + y; }
+ */
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        final int a = 2;
+        final int b = 3;
+        final int c = 4;
+
+        Function<Integer, Double> function1 = x -> a * Math.pow(x, 2) + b * x + c;
+        System.out.println(function1.apply(7));
+
+    }
+}
---
+++ src/com/tsekhanovich/functional/practice6/Task2.java
@@ -0,0 +1,26 @@
+package com.tsekhanovich.functional.practice6;
+
+import java.util.function.Function;
+
+/**
+ * @author Pavel Tsekhanovcih 10.11.2018
+ *
+ * Using closure write a lambda expression that adds prefix (before) and suffix (after) to its single string argument;
+ * prefix and suffix are final variables and will be available in the context during testing.
+ * All whitespaces on the both ends of the argument must be removed. Do not trim prefix, suffix and the result string.
+ * Solution format. Submit your lambda expression in any valid format with ; on the end.
+ *
+ * Examples: (x, y) -> x + y; (x, y) -> { return x + y; }
+ */
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        String prefix = "<";
+        String suffix = ">";
+
+        Function<String, String> example1 = s -> prefix + s.trim() + suffix;
+        System.out.println(example1.apply("Test"));
+
+    }
+}
