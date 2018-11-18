75351768998094e164053552b31e1cb20646bd68
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-11-18 17:35:41 +0300

    Added practice 11

---
+++ src/com/tsekhanovich/functional/practice11/Task1.java
@@ -0,0 +1,27 @@
+package com.tsekhanovich.functional.practice11;
+
+import java.util.function.Consumer;
+import java.util.function.IntUnaryOperator;
+
+/**
+ * @author Pavel Tsekhanovcih 11.11.2018
+ */
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        IntUnaryOperator mult2 = num -> num * 2;
+        IntUnaryOperator add3 = num -> num + 3;
+
+        IntUnaryOperator combinedOperator = add3.compose(mult2.andThen(add3)).andThen(mult2);
+        int result = combinedOperator.applyAsInt(5);
+        System.out.println(result);
+
+        Consumer<Integer> printer = System.out::println;
+        Consumer<Integer> devNull = (val) -> { int v = val * 2; };
+
+        Consumer<Integer> combinedConsumer = devNull.andThen(devNull.andThen(printer));
+        combinedConsumer.accept(100);
+
+    }
+}
