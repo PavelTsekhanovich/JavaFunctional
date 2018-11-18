4d097b7ff71e58be19fd8f5d5c4ea9ce05f1a129
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-11-18 17:33:36 +0300

    Added practice 9

---
+++ src/com/tsekhanovich/functional/practice9/Task1.java
@@ -0,0 +1,87 @@
+package com.tsekhanovich.functional.practice9;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * @author Pavel Tsekhanovcih 11.11.2018
+ *
+ * You have a class Account (number: String, balance: Long, isLocked: boolean), the list accounts of type
+ * List<Account> and the method filter(List<T> elems, Predicate<T> predicate) for filtering the given list of type
+ * T by the predicate.
+ * The class Account has the next methods: getNumber(), getBalance(), isLocked()
+ * for getting the values of the corresponding fields.
+ *
+ * Write a code for filtering the accounts list in two ways:
+ * - get list with all non-empty accounts (balance > 0) and save it to the variable nonEmptyAccounts
+ * - get all non-locked accounts with too much money (balance >= 100 000 000) and save
+ *   it to the variable accountsWithTooMuchMoney
+ *
+ * The class Account, the list accounts (List<Account>) and the method filter(...) will be available during testing.
+ * Important. Use the given code template for your solution. Do not change it!
+ *
+ * Example of use the filter method. The code below gets only even number from the list.
+ */
+
+public class Task1 {
+
+    public static final List<Account> ACCOUNTS = new ArrayList<>();
+
+    static {
+        ACCOUNTS.add(new Account("NO1", 100L, true));
+        ACCOUNTS.add(new Account("NO2", 30L, false));
+        ACCOUNTS.add(new Account("NO3", 10000000L, true));
+    }
+
+    private static <T> List<T> filter(List<T> inputList, Predicate<T> predicate) {
+        return inputList.stream().filter(predicate).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<Account> nonEmptyAccounts = filter(ACCOUNTS, x -> x.getBalance() > 0);
+        List<Account> accountsWithTooMuchMoney = filter(ACCOUNTS, x
+                -> x.getBalance() >= 100000000 && !x.isLocked());
+
+        nonEmptyAccounts.stream().forEach(System.out::println);
+        accountsWithTooMuchMoney.stream().forEach(System.out::println);
+    }
+}
+
+class Account {
+
+    private String number;
+    private Long balance;
+    private boolean isLocked;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void setLocked(boolean locked) {
+        isLocked = locked;
+    }
+
+    public Account(String number, Long balance, boolean isLocked) {
+        this.number = number;
+        this.balance = balance;
+        this.isLocked = isLocked;
+    }
+}
---
+++ src/com/tsekhanovich/functional/practice9/Task2.java
@@ -0,0 +1,35 @@
+package com.tsekhanovich.functional.practice9;
+
+import java.util.Objects;
+
+/**
+ * @author Pavel Tsekhanovcih 11.11.2018
+ *
+ * You need to write your own functional interface (TernaryIntPredicate) and use it with a lambda expression.
+ * The interface must have a single non-static (and non-default)
+ * method test with three int arguments that returns boolean value.
+ * Besides, you need to write a lambda expression with three int arguments using your TernaryIntPredicate.
+ *
+ * The lambda expression has to return true if all passed values are different otherwise false.
+ * The name of the instance is allValuesAreDifferentPredicate. It should be a static field.
+ *
+ * Important. Use the provided template for your solution. Do not change it!
+ */
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        TernaryIntPredicate<Integer, Integer, Integer, Boolean> function = (a, b, c) ->
+                !Objects.equals(a, b) &&
+                        !Objects.equals(b, c) &&
+                        !Objects.equals(c, a);
+        System.out.println(function.test(1, 2, 3));
+        System.out.println(function.test(1, 1, 1));
+    }
+}
+
+@FunctionalInterface
+interface TernaryIntPredicate<A, B, C, R> {
+
+    R test(A a, B b, C c);
+}
