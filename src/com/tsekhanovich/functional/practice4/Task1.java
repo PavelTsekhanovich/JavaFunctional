ae485e9369d862a7d477fcf1bb7f0624c3a04533
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-11-18 17:31:06 +0300

    Added practice 4

---
+++ src/com/tsekhanovich/functional/practice4/Task1.java
@@ -0,0 +1,24 @@
+package com.tsekhanovich.functional.practice4;
+
+import java.util.function.BiFunction;
+import java.util.stream.LongStream;
+
+/**
+ * @author Pavel Tsekhanovcih 10.11.2018
+ *
+ * Write a lambda expression that accepts two long arguments as a range borders and
+ * calculates (returns) production of all numbers in this range (inclusively).
+ * It's guaranteed that 0 <= left border <= right border. if left border == right border then the result is any border.
+ * Solution format. Submit your lambda expression in any valid format with ; on the end.
+ *
+ * Examples: (x, y) -> x + y; (x, y) -> { return x + y; };
+ */
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        BiFunction<Long, Long, Long> function = (x, y) ->
+                LongStream.rangeClosed(x, y).reduce(1L, Math::multiplyExact);
+        System.out.println(function.apply(3L, 5L));
+    }
+}
