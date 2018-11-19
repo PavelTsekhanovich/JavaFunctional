253f4263e1209ac67f6fdc2f4bc200a5eb8da140
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-11-19 10:15:34 +0300

    Added practice 12

---
+++ src/com/tsekhanovich/functional/practice12/Task1.java
@@ -0,0 +1,27 @@
+package com.tsekhanovich.functional.practice12;
+
+import java.util.List;
+import java.util.function.IntPredicate;
+
+/**
+ * @author Pavel Tsekhanovcih 11.11.2018
+ *
+ * Write the disjunctAll method that accepts a list of IntPredicate's and returns a single IntPredicate.
+ * The result predicate is a disjunction of all input predicates.
+ *
+ * If the input list is empty then the result predicate should return false for any integer value (always false).
+ *
+ * Important. Pay attention to the provided method template. Do not change it.
+ */
+
+public class Task1 {
+
+    /**
+     * The method represents a disjunct operator for a list of predicates.
+     * For an empty list it returns the always false predicate.
+     */
+    public static IntPredicate disjunctAll(List<IntPredicate> predicates) {
+        return predicates.stream().reduce(value -> false, IntPredicate::or);
+    }
+
+}
