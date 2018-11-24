e1c8c7b832d9dbc0d20b898165744fc3849689ab
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-11-24 23:41:14 +0300

    Added practice 26

---
+++ src/com/tsekhanovich/functional/practice26/Task1.java
@@ -0,0 +1,23 @@
+package com.tsekhanovich.functional.practice26;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Pavel Tsekhanovcih 13.11.2018
+ *
+ * Write a collector that evaluates the product of squares of integer numbers in a Stream<Integer>.
+ *
+ * Important. You should write only the collector in any valid formats but without ; on the end.
+ * It will be passed as an argument to the collect() method of a stream as shown below.
+ */
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(0, 1, 2, 3);
+        long val = numbers.stream().collect(Collectors.reducing(1, Math::multiplyExact));
+        System.out.println(val);
+    }
+}
